4c735ab099fbbf8f1f319161e9e027db470f374e
Jacob Doudna <jacob.doudna@jacks.sdstate.edu>
2021-02-17 20:08:46 -0600

    Added additional routing and Specialty Pizza Purchase

---
+++ lib/SpecialtyPizzas.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:pizza_ordering_app/SpecialtyPizzaPurchase.dart';
+
+class SpecialtyPizzasMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Perilous Pizza'),
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 122, 0, 0),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.shopping_cart_rounded, color: Colors.white),
+              onPressed: () {
+                //do something
+                print("shopping cart has been clicked");
+                Navigator.pushNamed(context, '/ShoppingCart');
+              },
+            )
+          ]),
+      body: Container(
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage('Assets/appBackground.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 40.0),
+                child: FlatButton(
+                  onPressed: () {
+                    print('Specialty Pizzas has been clicked');
+                    Navigator.pushNamed(context, '/SpecialtyPizzaPurchase');
+                  },
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Image.asset(
+                            'Assets/SupremePizza.jpg',
+                            height: 250.0,
+                            width: 300.0,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 240,
+                        width: 300,
+                        alignment: Alignment.bottomCenter,
+                        child: Text(
+                          'Supreme Pizza',
+                          style: TextStyle(
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                            shadows: [
+                              Shadow(
+                                  // bottomLeft
+                                  offset: Offset(-1.5, -1.5),
+                                  color: Colors.black),
+                              Shadow(
+                                  // bottomRight
+                                  offset: Offset(1.5, -1.5),
+                                  color: Colors.black),
+                              Shadow(
+                                  // topRight
+                                  offset: Offset(1.5, 1.5),
+                                  color: Colors.black),
+                              Shadow(
+                                  // topLeft
+                                  offset: Offset(-1.5, 1.5),
+                                  color: Colors.black),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                child: FlatButton(
+                  onPressed: () {
+                    print('Specialty Pizza Has Been Clicked');
+                    Navigator.pushNamed(context, '/SpecialtyPizzaPurchase');
+                  },
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Image.asset(
+                            'Assets/BuildYourOwn.jpg',
+                            height: 250.0,
+                            width: 300.0,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 240,
+                        width: 300,
+                        alignment: Alignment.bottomCenter,
+                        child: Text(
+                          'Goat Pizza',
+                          style: TextStyle(
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                            shadows: [
+                              Shadow(
+                                  // bottomLeft
+                                  offset: Offset(-1.5, -1.5),
+                                  color: Colors.black),
+                              Shadow(
+                                  // bottomRight
+                                  offset: Offset(1.5, -1.5),
+                                  color: Colors.black),
+                              Shadow(
+                                  // topRight
+                                  offset: Offset(1.5, 1.5),
+                                  color: Colors.black),
+                              Shadow(
+                                  // topLeft
+                                  offset: Offset(-1.5, 1.5),
+                                  color: Colors.black),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
